49f0911cf00320674a28c2dccd685c3549fc3ffe
Vadik-Konovalov <vkonovalov1771.vk@gmail.com>
2023-04-04 14:28:41 +0300

    hw06

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,15 +1,15 @@
 const getCurrent = async (req, res) => {
-    const { email, subscription } = req.user;
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        user: {
-          email,
-          subscription,
-        },
+  const { email, subscription } = req.user;
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      user: {
+        email,
+        subscription,
       },
-    });
-  };
-  
-  module.exports = getCurrent;
+    },
+  });
+};
+
+module.exports = getCurrent;
--- routers/api/users.js
+++ routers/api/users.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const { auth, upload } = require("../../middlewares");
 
-const { getCurrent, updateAvatar } = require("../../controllers/users");
+const { getCurrent, updateAvatar, verifyEmail } = require("../../controllers/users");
 
 const router = express.Router();
 
 router.get("/current", auth, getCurrent);
 router.patch("/avatars", auth, upload.single("avatar"), updateAvatar);
+router.get("/verify/:verificationToken", verifyEmail);
 
 module.exports = router;
